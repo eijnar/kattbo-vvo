6ba0eb1be180f2f2123cf453cf05e22a159b3ac2
Johan Andersson <github@kaffesump.se>
2023-12-13 21:25:48 +0100

    refacotring stuff..

--- app/blueprints/news/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, TextAreaField, SelectMultipleField, SubmitField
-from wtforms.validators import DataRequired
-
-class PostForm(FlaskForm):
-    title = StringField('Titel', validators=[DataRequired()])
-    content = TextAreaField(validators=[DataRequired()])
-    tags = SelectMultipleField('Taggar')
-    submit = SubmitField()
