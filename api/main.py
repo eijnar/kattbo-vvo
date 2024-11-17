e577d64e25b30c41b0211e5de214c67a471e185c
Johan Andersson <johan@morbit.se>
2024-11-17 21:02:03 +0100

    bind api to localhost, not all interfaces

--- api/main.py
+++ api/main.py
@@ -88,7 +88,7 @@ def main():
     app = create_app()
 
     import uvicorn
-    uvicorn.run("main:create_app", host="0.0.0.0",
+    uvicorn.run("main:create_app", host="127.0.0.1",
                 port=8000, reload=True, factory=True)
 
 
