d642641ed613fabd8b34e9c7ead2c994dcb4b50f
Johan Andersson <johan@morbit.se>
2024-11-17 23:03:03 +0100

    changed uvicorn to run on socket

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -3,9 +3,11 @@ set -e
 
 # Make sure the logs directory exists
 mkdir -p /app/logs
+mkdir -p /app/socket
 
 # Change ownership of the logs directory
 chown -R appuser:appuser /app/logs
+chown -R appuser:appuser /app/socket
 
 # Execute the CMD as appuser
 exec gosu appuser "$@"
