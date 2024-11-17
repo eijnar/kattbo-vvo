3d2ca57b6776035c62983a7eca8407559641e0ba
Johan Andersson <johan@morbit.se>
2024-11-18 00:05:41 +0100

    changed back to using standard port

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -3,11 +3,9 @@ set -e
 
 # Make sure the logs directory exists
 mkdir -p /app/logs
-mkdir -p /app/socket
 
 # Change ownership of the logs directory
 chown -R appuser:appuser /app/logs
-chown -R appuser:appuser /app/socket
 
 # Execute the CMD as appuser
 exec gosu appuser "$@"
