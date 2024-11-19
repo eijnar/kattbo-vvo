2c7ba148c668c4a316cf7345b30b413b7750a22f
Johan Andersson <johan@morbit.se>
2024-11-19 23:00:38 +0100

    Verification of aud is now done before since it can be a list

--- api/core/security/jwt.py
+++ api/core/security/jwt.py
@@ -37,13 +37,33 @@ def decode_jwt(token: str):
         raise HTTPException(
             status_code=401, detail="Unable to find appropriate key")
     try:
+        # Decode the token without verifying the signature to extract claims
+        unverified_payload = jwt.decode(
+            token,
+            key=None,  # No key is needed since we're not verifying the signature yet
+            algorithms=settings.ALGORITHMS,
+            options={"verify_signature": False}
+        )
+
+        # Extract the 'aud' claim and ensure it's a list
+        token_audience = unverified_payload.get('aud')
+        if isinstance(token_audience, str):
+            token_audience = [token_audience]
+
+        # Verify that the expected audience is in the token's 'aud' claim
+        expected_audience = settings.API_AUDIENCE
+        if expected_audience not in token_audience:
+            raise JWTError('Invalid audience')
+
+        # Now decode the token with signature verification, skipping audience check
         payload = jwt.decode(
             token,
             rsa_key,
             algorithms=settings.ALGORITHMS,
-            audience=settings.API_AUDIENCE,
+            options={"verify_aud": False},
             issuer=f"https://{settings.AUTH0_DOMAIN}/"
         )
+
         return payload
     except JWTError as e:
         logger.error(f"Failure to decode token: {e}")
